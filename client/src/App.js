40d1357101b2d1b94e9635981154dbb509ac0168
Sean Dougherty <seandougherty358@gmail.com>
2021-01-28 17:45:47 +0000

    gang

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { listLogEntries} from './api';
+
 
 function App() {
+  const [logEntries, setLogentries] = useState([ ])
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 52.355518,
+    longitude: -1.174320,
+    zoom: 5
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogentries(logEntries);
+    })()
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={ setViewport()}
+      
+    >
+      {
+        logEntries.map(entry => (
+        
+          <Marker 
+            latitude={entry.latitude} 
+            longitude={entry.longitude} 
+            offsetLeft={-20} 
+            offsetTop={-10}>
+              <div>{entry.title}</div>
+          </Marker>
+        
+        ))
+      }
+      </ReactMapGL>
   );
+
 }
 
 export default App;
